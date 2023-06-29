073e7e80b137c1fab5b4d457cfa2423875fa766e
daniel-popov-newcross <daniel.popov@newcrosshealthcare.com>
2023-06-29 16:59:22 +0300

    Enable any cors origin

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:5173"
+  origin: "*" //"http://localhost:5173"
 };
 
 app.use(cors(corsOptions));
